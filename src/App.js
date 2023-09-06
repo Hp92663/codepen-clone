159968288c15b4460ab26c513375e4aee5330711
Hp92663 <hirenkhut92663@gmail.com>
2023-09-07 01:31:31 +0530

    Editor setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import Editor from './components/Editor'
 
 function App() {
   return (
     <>
       <div>
-          <div className="pane top-pane"></div>
+          <div className="pane top-pane">
+            <Editor />
+            <Editor />
+            <Editor />
+          </div>
           <div className="pane"></div>
           <iframe
           title="output"
           sandbox="allow-scripts"
           frameBorder='0'
-          
+          width='100%'
+          height='100%'
           />
       </div>
     </>
